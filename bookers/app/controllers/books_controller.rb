bc348c881dc4cb116fe8937ef6a405488ac10339
konmarii <chankonworld@gmail.com>
2021-07-08 08:15:47 +0000

    [update]topページのレイアウト作成

--- bookers/app/controllers/books_controller.rb
+++ bookers/app/controllers/books_controller.rb
@@ -1,7 +1,16 @@
 class BooksController < ApplicationController
+  def index
+    @book = Book.all
+  end
+
   def show
   end
 
   def edit
   end
+
+  private
+    def list_params
+      params.require(:list).permit(:title, :body)
+    end
 end
